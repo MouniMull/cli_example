32a5cb881f206c5f4ce8fb3eba7426c900cf49e6
Emily Wilder <emilywilder.earth@gmail.com>
2019-02-13 15:51:27 -0500

    add rest api functionality

--- CLI.rb
+++ CLI.rb
@@ -1,3 +1,7 @@
+require 'JSON'
+require 'pry'
+require 'rest-client'
+
 class CLI
 
   def initialize
@@ -6,6 +10,7 @@ class CLI
       'air' => 'Air is not dense.',
       'fire' => 'Fire is hot.'
     }
+    @api_url = "https://jsonplaceholder.typicode.com"
   end
 
   def start
@@ -20,6 +25,8 @@ class CLI
         break
       when 'learn'
         learn
+      when 'userposts'
+        user_posts
       end
     end
   end
@@ -31,6 +38,7 @@ class CLI
     puts 'sayhi - Say hello!'
     puts 'saygoodbye - Say goodbye!'
     puts 'learn - Choose topic to learn!'
+    puts "userposts - Get information about a user's posts!"
     print '> '
   end
 
@@ -61,6 +69,40 @@ class CLI
         break
       end
     end
+  end
 
+  def get_data(url)
+    results = RestClient.get(url)
+    JSON.parse(results)
+  end
+
+  def user_posts
+    users = get_data("#{@api_url}/users")
+    users = users.first(5)
+    loop do
+      puts 'Choose a user!'
+      puts
+      users.each do |user|
+        puts user["name"]
+      end
+      print '> '
+      username = get_input
+      user = users.find { |user| user["name"] == username }
+      if user
+        show_user_titles(user)
+      else
+        break
+      end
+    end
+  end
+
+  def show_user_titles(user)
+    titles = get_data("#{@api_url}/posts?userId=#{user["id"]}")
+    titles = titles.first(5)
+    puts "Titles by #{user["name"]}:"
+    puts
+    titles.each do |title|
+      puts title["title"]
+    end
   end
 end
