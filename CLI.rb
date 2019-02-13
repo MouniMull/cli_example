4caa6e748c44272d775b40501485c9053366bf37
Emily Wilder <emilywilder.earth@gmail.com>
2019-02-13 14:48:59 -0500

    add learn method

--- CLI.rb
+++ CLI.rb
@@ -1,4 +1,13 @@
 class CLI
+
+  def initialize
+    @learntopics = {
+      'snow' => 'Snow is cold.',
+      'air' => 'Air is not dense.',
+      'fire' => 'Fire is hot.'
+    }
+  end
+
   def start
     loop do
       greet
@@ -9,6 +18,8 @@ class CLI
       when 'saygoodbye'
         saygoodbye
         break
+      when 'learn'
+        learn
       end
     end
   end
@@ -19,6 +30,7 @@ class CLI
     puts
     puts 'sayhi - Say hello!'
     puts 'saygoodbye - Say goodbye!'
+    puts 'learn - Choose topic to learn!'
     print '> '
   end
 
@@ -32,5 +44,23 @@ class CLI
 
   def saygoodbye
     puts "Goodbye!"
+  end
+
+  def learn
+    loop do
+      puts 'Choose an adventure!'
+      puts
+      @learntopics.keys.each do |topic|
+        puts topic
+      end
+      print '> '
+      input = get_input
+      if @learntopics.has_key?(input)
+        puts "WISDOM: #{@learntopics[input]}"
+      else
+        break
+      end
+    end
+
   end
 end
